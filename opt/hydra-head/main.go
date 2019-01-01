a1da117d5fc74df32f3e2ab2016183724d3cef0a
Eeo Jun <141bytes@gmail.com>
2019-01-01 22:54:19 +0000

    simplify hydra-head

--- opt/hydra-head/main.go
+++ opt/hydra-head/main.go
@@ -1,27 +1,21 @@
 package main
 
 import "time"
-import "bufio"
 import "os"
 import "net"
 
 func main() {
-	attempts := 5
-	delay := 50 * time.Millisecond
-
-	for i := 0; i < attempts; i++ {
-		conn, err := net.Dial("tcp", "localhost:9900")
-		if err != nil {
-			time.Sleep(delay)
-			delay *= 2
-			continue
-		}
-		defer conn.Close()
-		r := bufio.NewScanner(conn)
-		for r.Scan() {
-			_, _ = os.Stdout.Write(append(r.Bytes(), '\n'))
-		}
-		return
+	conn, err := net.DialTimeout("tcp", "localhost:9900", 500*time.Millisecond)
+	if err != nil {
+		os.Exit(1)
 	}
-	os.Exit(1)
+	b := make([]byte, 256)
+	defer conn.Close()
+	for {
+		n, err := conn.Read(b)
+		_, _ = os.Stdout.Write(b[:n])
+		if err != nil {
+			return
+		}
+	}
 }
